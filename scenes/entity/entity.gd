d1d81147752cf1867768d1aa2e9f604f4ae6bcac
Koh Jia Xian <kohjx96@gmail.com>
2023-12-10 01:28:12 +0800

    hotfix for movement stopping on NW and NE corners of planet

--- scenes/entity/entity.gd
+++ scenes/entity/entity.gd
@@ -7,9 +7,18 @@ func _ready():
 	set_floor_max_angle(deg_to_rad(360))
 
 func _physics_process(delta):
+	var temp = velocity.normalized()
 	if not is_on_floor():
 		velocity = gravity * delta
-	print(move_and_slide())
+	move_and_slide()
+	
+	# Hotfix for bug where tank gets stuck
+	if velocity == Vector2():
+		if temp.x > 0:
+			velocity = Vector2(1000, 0)
+		else:
+			velocity = Vector2(-1000, 0)
+		move_and_slide()
 
 # Handle velocity from gravity
 func handle_gravity_velocity(delta):
--- scenes/entity/player/player.gd
+++ scenes/entity/player/player.gd
@@ -1,13 +1,11 @@
 extends "res://scenes/entity/entity.gd"
 
-const SPEED = 25000.0
+const SPEED = 35000.0
 
 func _physics_process(delta):
 	reset_physics()
 	handle_gravity_rotation()
-	
 	handle_input_velocity(delta)
-	
 	handle_gravity_velocity(delta)
 	super._physics_process(delta)
 
@@ -17,7 +15,12 @@ func handle_input_velocity(delta):
 					Input.get_action_strength("ui_left")
 	var input_vector = SPEED * delta * input_dir * Vector2.RIGHT
 	
+	# Move angle slightly upwards to minimise collision with planet
+	if input_dir > 0:
+		input_vector = input_vector.rotated(deg_to_rad(-5))
+	elif input_dir < 0:
+		input_vector = input_vector.rotated(deg_to_rad(5))
+	
 	# rotate to current rotation's axis
 	velocity += input_vector.rotated(rotation)
-	print(velocity)
 	
