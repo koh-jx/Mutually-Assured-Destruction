f56f90615ae121bbf05f49d9294e98702d353878
Koh Jia Xian <kohjx96@gmail.com>
2023-12-11 14:15:26 +0800

    add rotation of shot direction using W-S keys

--- scenes/Projectiles/missle.gd
+++ scenes/Projectiles/missle.gd
@@ -1,7 +1,7 @@
 extends "res://scenes/Projectiles/projectile.gd"
 
 const magnitude = 450.0
-const angle = 30.0
+const damage = 20.0
 
 func _ready():
 	#thrust_towards(Vector2()) # Example to use if placed directly in world
@@ -20,15 +20,9 @@ func _integrate_forces(state):
 func _on_body_entered(body):
 	queue_free()
 
-func _on_hitbox_body_entered(body):
-	# TODO Damage entity
-	queue_free()
-
 func _on_invuln_timer_timeout():
 	toggle_collision()
 	
 func toggle_collision():
 	var shape = $CollisionShape2D
 	shape.set_disabled(!shape.disabled)
-	var hitboxShape = $Hitbox/CollisionShape2D
-	hitboxShape.set_disabled(!hitboxShape.disabled)
